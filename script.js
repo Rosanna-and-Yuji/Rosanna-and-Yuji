0d57599d934041b3be59f06907760b97d0ee56db
Rosanna-and-Yuji <power_flower5@hotmail.com>
2025-03-02 21:32:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -64,30 +64,3 @@ updateCountdown();
         plusOneSelect.addEventListener("change", togglePlusOneName);
     }
 });
-
-// Get modal element
-const modal = document.getElementById('rsvpModal');
-
-// Get the button that opens the modal
-const openModalBtn = document.getElementById('openModalBtn');
-
-// Get the <span> element that closes the modal
-const closeModalBtn = document.getElementById('closeModalBtn');
-
-// When the user clicks the button, open the modal
-openModalBtn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-closeModalBtn.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
